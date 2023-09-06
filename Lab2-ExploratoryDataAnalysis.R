21070abbca0fa9c32e4ef37939388adccfb20f6f
pear_05 <waswaustin@gmail.com>
2023-09-06 16:28:08 +0300

    Pull request

--- Lab2-ExploratoryDataAnalysis.R
+++ Lab2-ExploratoryDataAnalysis.R
@@ -15,7 +15,7 @@
 # License: GNU GPL-3.0-or-later
 # See LICENSE file for licensing information.
 # *****************************************************************************
-#Austin  Test commit
+#Austin  Test commit and create pull
 # STEP 1. Install and use renv ----
 # **Initialization: Install and use renv ----
 # The renv package helps you create reproducible environments for your R
