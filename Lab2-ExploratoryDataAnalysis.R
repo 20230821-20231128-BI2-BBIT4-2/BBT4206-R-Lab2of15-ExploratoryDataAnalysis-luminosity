c2623417745e73eebacaf7bcb86b5b56771e3f0d
pear_05 <waswaustin@gmail.com>
2023-09-13 17:14:59 +0300

    lab2

--- Lab2-ExploratoryDataAnalysis.R
+++ Lab2-ExploratoryDataAnalysis.R
@@ -73,7 +73,6 @@ renv::init()
 # Execute the following code to reinstall the specific package versions
 # recorded in the lockfile:
 renv::restore()
-
 # One of the packages required to use R in VS Code is the "languageserver"
 # package. It can be installed manually as follows if you are not using the
 # renv::restore() command.
